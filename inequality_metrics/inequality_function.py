35e3b62ce1b5d2d72b4a32ef1d336d23444b05da
Michael Freeman <michaelfreeman100@gmail.com>
2020-08-20 12:56:51 +1200

    Fixed up fucntions and removed unwanted ones, added Mitchell as author

--- inequality_metrics/inequality_function.py
+++ inequality_metrics/inequality_function.py
@@ -68,58 +68,7 @@ def calc_kappa(a, beta, weights = None):
 # Other inequality functions
 ####
 
-def atkinson_adjusted_ede(a, beta = -0.5, weight = None):
-    """
-    Params:
-        a: Distribution of data; List
-        beta: Set inequality aversion parameter; Integer
-        weights: For weighted average; List (Length of a)
-
-    Returns: Adjusted Atkinson EDE
-    """
-    ede_sum = 0
-    if not weight:
-        N = len(a)
-    else:
-        N = sum(weight)
-    x_mean = np.average(a, weights = weight)
-    count = 0
-    for i in a:
-        if not weight:
-            ede_sum += (i**(1-beta))
-        else:
-            ede_sum += (i**(1-beta)) * weight[count]
-        count += 1
-    ede = (ede_sum / N)**(1 / (1 - beta))
-    return(ede)
-
-def atkinson_adjusted_index(a, beta = -0.5, weight = None):
-    """
-    Params:
-        a: Distribution of data; List
-        beta: Set inequality aversion parameter; Integer
-        weights: For weighted average; List (Length of a)
-
-    Returns: Adjusted Atkinson index
-    """
-    ede_sum = 0
-    if not weight:
-        N = len(a)
-    else:
-        N = sum(weight)
-    x_mean = np.average(a, weights = weight)
-    count = 0
-    for i in a:
-        if not weight:
-            ede_sum += (i**(1-beta))
-        else:
-            ede_sum += (i**(1-beta)) * weight[count]
-        count += 1
-    ede = (ede_sum / N)**(1 / (1 - beta))
-    index = (ede / x_mean) - 1
-    return(index)
-
-def atkinson_ede(a, epsilon = -0.5, weight = None):
+def atkinson_ede(a, epsilon = -0.5, weights = None):
     """
     Params:
         a: Distribution of data; List
@@ -131,22 +80,22 @@ def atkinson_ede(a, epsilon = -0.5, weight = None):
     NOTE: this is meant for distributions where high values are better
     """
     at_sum = 0
-    if not weight:
+    if not weights:
         N = len(a)
     else:
-        N = sum(weight)
-    x_mean = np.average(a, weights = weight)
+        N = sum(weights)
+    x_mean = np.average(a, weights = weights)
     count = 0
     for i in a:
-        if not weight:
+        if not weights:
             at_sum += i**(1 - epsilon)
         else:
-            at_sum += (i**(1 - epsilon)) * weight[count]
+            at_sum += (i**(1 - epsilon)) * weights[count]
         count += 1
     ede = (at_sum / N)**(1 / (1 - epsilon))
     return(ede)
 
-def atkinson_index(a, epsilon = -0.5, weight = None):
+def atkinson_index(a, epsilon = -0.5, weights = None):
     """
     Params:
         a: Distribution of data; List
@@ -156,23 +105,23 @@ def atkinson_index(a, epsilon = -0.5, weight = None):
     Returns: Atkinson index
     """
     ede_sum = 0
-    if not weight:
+    x_mean = np.average(a, weights = weights)
+    if not weights:
         N = len(a)
     else:
-        N = sum(weight)
-    x_mean = np.average(a, weights = weight)
+        N = sum(weights)
     count = 0
     for i in a:
-        if not weight:
+        if not weights:
             ede_sum += (i**(1-epsilon))
         else:
-            ede_sum += (i**(1-epsilon)) * weight[count]
+            ede_sum += (i**(1-epsilon)) * weights[count]
         count += 1
     ede = (ede_sum / N)**(1 / (1 - epsilon))
     index = 1 - (ede / x_mean)
     return(index)
 
-def gini_index(a, beta = -0.5, weight = None):
+def gini_index(a, beta = -0.5, weights = None):
     """
     Params:
         a: Distribution of data; List
@@ -181,33 +130,31 @@ def gini_index(a, beta = -0.5, weight = None):
         
     Returns: Gini index
     """
-    area_total = simps(np.arange(0,101,1), dx=1)
-    if not weight:
-        N = len(a)
-    else:
-        N = sum(weight)
     a = list(np.sort(a))
     a_percent = []
     weight_perc = []
     w_perc_sum = 0
     perc_sum = 0
-    if not weight:
-        for i in a:
-            perc_sum += i/data_tot*100
-            a_percent.append(perc_sum)
+    data_tot = sum(a)
+    area_total = simps(np.arange(0,101,1), dx=1)
+    if not weights:
+        N = len(a)
+    else:
+        N = sum(weights)
+    for i in a:
+        perc_sum += i/data_tot*100
+        a_percent.append(perc_sum)
+    if not weights:
         area_real = simps(a_percent)
         area_diff = area_total - area_real
         gini = round((area_diff / area_total), 3)
     else:
-        weight_tot = sum(weight)
-        data_tot = sum(a)
-        for i in a:
-            perc_sum += i/data_tot*100
-            a_percent.append(perc_sum)
-        for i in weight:
+        weight_tot = sum(weights)
+        for i in weights:
             w_perc_sum += i/weight_tot*100
             weight_perc.append(w_perc_sum)
         area_real = simps(a_percent, weight_perc)
         area_diff = area_total - area_real
         gini = round((area_diff / area_total), 3)
     return(gini)
+
