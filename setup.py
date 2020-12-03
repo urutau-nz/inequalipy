0bda1addb513645482ce8fabbd62b27c91e599ff
Michael Freeman <michael@rubyplaynetwork.com>
2020-12-03 14:44:58 +1300

    Updated the setup.py, can now import module but you cant use the functions >:(

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as readme_file:
 
 setuptools.setup(
     name="InequalityMetrics",
-    version="1.0.4",
+    version="1.0.6",
     author="Tom Logan, Mitchell Anderson",
     author_email="tom.logan@canterbury.ac.nz",
     description="Inequality Metrics contains functions for the Kolm-Pollak, Atkinson EDE and Gini Index aproaches to calculating inequality distributions",
@@ -14,7 +14,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/urutau-nz/inequality-metrics",
     packages=setuptools.find_packages(),
-    package_dir = {"InequalityMetrics": "inequality_metrics"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
